fccdf6e6a45fa1e8a5266ab7c6a62b65b80db68f
Lusca Lima <lucas@siasp.com.br>
2020-08-18 22:59:54 -0300

    refactor: changing the routes of task to authentication

--- src/routes/TaskRoutes.ts
+++ src/routes/TaskRoutes.ts
@@ -1,20 +1,21 @@
 import { Router } from 'express'
 const router = Router()
 import Task from '../controllers/TaskController'
+import { auth } from '../middlewares/auth'
 
 /** Criation route */
-router.post('/', Task.create)
+router.post('/', auth, Task.create)
 
 /** Get all tasks */
-router.get('/', Task.all)
+router.get('/', auth, Task.all)
 
 /** Get one task by ID */
-router.get('/:id', Task.oneById)
+router.get('/:id', auth, Task.oneById)
 
 /** Update one task by ID */
-router.put('/:id', Task.update)
+router.put('/:id', auth, Task.update)
 
 /** Delete one task by ID */
-router.delete('/:id', Task.delete)
+router.delete('/:id', auth, Task.delete)
 
 export default router
